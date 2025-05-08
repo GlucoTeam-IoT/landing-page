66cd38a2a1ebe43a8a1d6f2093b8a607ba81aeef
Jose Calderon <u202213076@upc.edu.pe>
2025-05-08 02:58:33 -0500

    refactor: reorganize Landing component structure for improved readability

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,10 @@
 import About from "../components/sections/About";
+import ContactUs from "../components/sections/ContactUs";
 import DownloadApp from "../components/sections/DownloadApp";
 import Hero from "../components/sections/Hero";
 import KeyFeatures from "../components/sections/KeyFeatures";
+import Team from "../components/sections/Team";
+import Footer from "../components/ui/Footer";
 import Navbar from "../components/ui/Navbar";
 
 const Landing = () => {
@@ -13,6 +16,13 @@ const Landing = () => {
         <About />
         <KeyFeatures />
         <DownloadApp />
+        <Team />
+      </div>
+      <div className="bg-gray-50">
+        <ContactUs />
+      </div>
+      <div className="bg-xd-blue">
+        <Footer />
       </div>
     </>
   );
