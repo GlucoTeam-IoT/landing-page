a253a388c9b8bed9b5cf1a53dc13cf0bc26161b7
Jose Calderon <u202213076@upc.edu.pe>
2025-05-14 01:39:34 -0500

    refactor: change routes for login and register

--- src/utils/consts/navbar.ts
+++ src/utils/consts/navbar.ts
@@ -1,66 +1,66 @@
 export interface NavItem {
-    id: string;
-    label: string;
+  id: string;
+  label: string;
+  href: string;
+  isButton?: boolean;
+}
+
+export interface NavbarConfig {
+  logo: {
+    src: string;
+    alt: string;
     href: string;
-    isButton?: boolean;
-  }
-  
-  export interface NavbarConfig {
-    logo: {
-      src: string;
-      alt: string;
-      href: string;
-    };
-    navItems: NavItem[];
-  }
-  
-  export const navbarConfig: NavbarConfig = {
-    logo: {
-      src: "/icons/glucova.png",
-      alt: "Company Logo",
-      href: "/"
-    },
-    navItems: [
-      {
-        id: "home",
-        label: "Home",
-        href: "#home"
-      },
-      {
-        id: "about",
-        label: "About",
-        href: "#about"
-      },
-      {
-        id: "features",
-        label: "Features",
-        href: "#features"
-      },
-      {
-        id: "download",
-        label: "Download",
-        href: "#download"
-      },
-      {
-        id: "team",
-        label: "Team",
-        href: "#team"
-      },
-      {
-        id: "contact",
-        label: "Contact",
-        href: "#contact"
-      },
-      {
-        id: "login",
-        label: "Login",
-        href: "/login"
-      },
-      {
-        id: "signup",
-        label: "Sign Up",
-        href: "/signup",
-        isButton: true
-      }
-    ]
   };
+  navItems: NavItem[];
+}
+
+export const navbarConfig: NavbarConfig = {
+  logo: {
+    src: "/icons/glucova.png",
+    alt: "Company Logo",
+    href: "/",
+  },
+  navItems: [
+    {
+      id: "home",
+      label: "Home",
+      href: "#home",
+    },
+    {
+      id: "about",
+      label: "About",
+      href: "#about",
+    },
+    {
+      id: "features",
+      label: "Features",
+      href: "#features",
+    },
+    {
+      id: "download",
+      label: "Download",
+      href: "#download",
+    },
+    {
+      id: "team",
+      label: "Team",
+      href: "#team",
+    },
+    {
+      id: "contact",
+      label: "Contact",
+      href: "#contact",
+    },
+    {
+      id: "login",
+      label: "Login",
+      href: "https://glucovaapp.netlify.app/",
+    },
+    {
+      id: "signup",
+      label: "Sign Up",
+      href: "https://glucovaapp.netlify.app/",
+      isButton: true,
+    },
+  ],
+};
